f00b6847c474d37f8666f632ba49ac55f90710ec
Patrick Hastings <phastings82@gmail.com>
2022-10-11 15:20:55 -0400

    added to create

--- FirstWebApp/Pages/ClientsList/Create.cshtml.cs
+++ FirstWebApp/Pages/ClientsList/Create.cshtml.cs
@@ -5,8 +5,31 @@ namespace FirstWebApp.Pages.ClientsList
 {
     public class CreateModel : PageModel
     {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public String SuccessMessage = "";
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||
+                clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                errorMessage = "All the fields are required";
+                return;
+            }
+
+            //save the new client into the database
+
+            clientInfo.name = ""; clientInfo.email = ""; clientInfo.phone = ""; clientInfo.address = "";
+            SuccessMessage = "New Client Added Correctly";
         }
     }
 }
