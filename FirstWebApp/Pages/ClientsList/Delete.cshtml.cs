bf324c00caf10f25090d9baaea123fe63d2aee18
Patrick Hastings <phastings82@gmail.com>
2022-10-12 12:11:38 -0400

    added delete

---
+++ FirstWebApp/Pages/ClientsList/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstWebApp.Pages.ClientsList
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
