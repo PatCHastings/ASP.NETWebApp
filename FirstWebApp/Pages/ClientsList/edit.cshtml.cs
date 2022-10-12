bdd9c3689f43ff6b35c3bce931d723e4a5c9bd49
Patrick Hastings <phastings82@gmail.com>
2022-10-12 10:17:18 -0400

    edit functional

--- FirstWebApp/Pages/ClientsList/edit.cshtml.cs
+++ FirstWebApp/Pages/ClientsList/edit.cshtml.cs
@@ -28,6 +28,10 @@ namespace FirstWebApp.Pages.ClientsList
                             if (reader.Read())
                             {
                                 clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2); 
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
                             }
                         }
                     }
@@ -42,7 +46,49 @@ namespace FirstWebApp.Pages.ClientsList
 
         public void OnPost()
         {
-            
+            clientInfo.id = Request.Form["id"];
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if (clientInfo.id.Length == 0 || clientInfo.name.Length == 0 || 
+                clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 ||
+                clientInfo.address.Length == 0)
+            {
+                errorMessage = "All fields are required";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "UPDATE clients " +
+                                 "SET name=@name, email=@email, phone=@phone, address=@address " +
+                                 "WHERE id=@id";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+                        command.Parameters.AddWithValue("@id", clientInfo.id);
+
+                        command.ExecuteNonQuery();  
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage=ex.Message;
+                return;
+            }
+
+            Response.Redirect("/ClientsList/Index");
         }
     }
 }
